7234e117bb9c291fbd051931c52e65e6b76eac74
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2022-11-28 00:06:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-sed -i 's/ImmortalWrt/G-DOCK/g' package/base-files/files/bin/config_generate
-
-sed -i 's/OpenWrt/PandoraBox/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
